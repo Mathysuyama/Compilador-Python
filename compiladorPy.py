4adbcb0f17a720a1bb804ebb10a4061766651cb4
Rivaldo Santos <mathysuyama@gmail.com>
2025-06-11 15:41:59 -0300

    criando a janela

--- compiladorPy.py
+++ compiladorPy.py
@@ -1,1 +1,8 @@
-pint("teste")
+import tkinter as tk  # Importa o Tkinter para criar a interface gráfica
+
+# Cria a janela principal
+root = tk.Tk()
+root.title("Compilador Python para Executável")  # Define o título da janela
+
+# Inicia o loop principal da interface
+root.mainloop()
